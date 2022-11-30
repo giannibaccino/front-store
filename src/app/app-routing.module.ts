817a2593ce59f3e0b6c8ed47c3436878f7454d28
giannibaccino <giannibaccino@gmail.com>
2022-11-30 15:36:29 -0300

    implements more components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CheckoutComponent } from './components/checkout/checkout.component';
-import { HomeComponent } from './components/home/home.component';
-import { ProductComponent } from './components/product/product.component';
+import { AboutComponent } from './pages/about/about.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProductComponent } from './pages/product/product.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'products', component: ProductComponent},
   {path: 'checkout', component: CheckoutComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'login', component: LoginComponent},
   {path: '**', redirectTo: 'home'}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { ProductComponent } from './components/product/product.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductComponent } from './pages/product/product.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
@@ -12,8 +12,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './pages/about/about.component';
+import { LoginComponent } from './pages/login/login.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { CheckoutComponent } from './components/checkout/checkout.component';
 
 
 @NgModule({
@@ -21,8 +24,11 @@ import { CheckoutComponent } from './components/checkout/checkout.component';
     AppComponent,
     HomeComponent,
     ProductComponent,
+    NavbarComponent,
     FooterComponent,
-    CheckoutComponent
+    CheckoutComponent,
+    AboutComponent,
+    LoginComponent
    ],
   imports: [
     BrowserModule,
